679621a952e12e632173fcfa9bdde7598642cfdf
Anderson Malaquias Da Silva <112710538+AndersonMSilva91@users.noreply.github.com>
2022-09-09 11:05:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const prompt = require ('prompt-sync')();
+
+const nome = prompt('Digite Seu Nome: ');
+
+const sobrenome = prompt('Digite Seu Sobrenome: ');
+
+const peso = prompt('Digite O Seu Peso: ');
+
+const altura = prompt('Digite Sua Altura: ');
+
+const dataInternacao = prompt('Data que deu entrada na intenção: ');
+
+const diabete = prompt ('Você é diabético? ')
+
+console.log(`\nNome Completo: ${nome}\nSeu sobrenome: ${sobrenome}\nPeso: ${peso}\nAltura: ${altura}\nData da Internação: ${dataInternacao}\nDiabético: ${diabete}`);
